4c63c4a1ff3ab99814364fd2e0e6cee88280c7ee
Muhammad Rafa Eka Pramoedya <91171858+TheR4V3@users.noreply.github.com>
2023-04-09 23:47:45 +0700

    meneyelesaikan program dengan runtime configuration

---
+++ tpmodul8_1302210123/CovidConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace tpmodul8_1302210123
+{
+    internal class CovidConfig
+    {
+        public string satuan_Suhu { get; set; }
+        public int batas_hari_demam { get; set; }
+        public string Pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+        public CovidConfig()
+        {
+            satuan_Suhu = "celcius";
+            batas_hari_demam = 3;
+            Pesan_ditolak = "Anda tidak lolos screening COVID-19.";
+            pesan_diterima = "Anda lolos screening COVID-19.";
+        }
+
+        public static void UbahSatuan(ref CovidConfig config)
+        {
+            if (config.satuan_Suhu == "celcius")
+            {
+                config.satuan_Suhu = "fahrenheit";
+            }
+            else if (config.satuan_Suhu == "fahrenheit")
+            {
+                config.satuan_Suhu = "celcius";
+            }
+            else
+            {
+                Console.WriteLine("Satuan suhu tidak valid!");
+            }
+        }
+
+    }
+}
--- tpmodul8_1302210123/Program.cs
+++ tpmodul8_1302210123/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8_1302210123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = File.ReadAllText("D:\\Code\\TPKPL\\TPMOD8\\tpmodul8_1302210123\\Covid_Config.json");
+            CovidConfig config = JsonSerializer.Deserialize<CovidConfig>(jsonString);
+
+            Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai " + config.satuan_Suhu);
+            double suhu = Convert.ToDouble(Console.ReadLine());
+            
+
+            Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+            int hariDemam = 0;
+            hariDemam = Convert.ToInt32(Console.ReadLine());
+                   
+            if (suhu >= 36.5 && suhu <= 37.5 && hariDemam >= config.batas_hari_demam)
+            {
+                Console.WriteLine(config.pesan_diterima);
+            }
+            else
+            {
+                Console.WriteLine(config.Pesan_ditolak);
+            }
+        }
+    }
+}
